792f25c096a3fd060369281811497f870a64a5c0
AthinaLange <athina.lange@ucdconnect.ie>
2017-07-06 16:19:57 +0100

    cleaned up transition

--- variable-trajectory.h
+++ variable-trajectory.h
@@ -4,13 +4,15 @@
 #include <gsl/gsl_rng.h>
 
 double ddd4, ddd, ppower, de; //indexed by trajectory index
-double **RR, **PP, *dhat, *f, *dgam, *Pperp /*perp element of momentum to d - position dependent*/; //trajectory dependent
-double abs_d /*trajectory dependent*/, Pdotdhat /*component of momentum parallel to d (function) trajectory-dependent*/;
+double **RR, **PP, *dhat, *f, *dgam, *Pperp /*perp element of momentum to d - position dependent*/;
+double abs_d /*trajectory dependent*/, Pdotdhat /*component of momentum parallel to d (function)*/;
 
 double sina, cosa, alpha; //trajectory dependent
 
-double  *abszsum0, *abszsum1, *argzsum0, *argzsum1, **realsum, **imagsum; //0 - summand trajectory dependent ; 1 - sum
-double  *habszsum0, *habszsum1, *hargzsum0, *hargzsum1, **hrealsum, **himagsum;
+double  abszsum0, argzsum0;
+double *abszsum1, *argzsum1, **realsum, **imagsum; //0 - summand trajectory dependent ; 1 - sum
+double  habszsum0, hargzsum0;
+double *habszsum1, *hargzsum1, **hrealsum, **himagsum;
 
 
 #endif
